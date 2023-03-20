6417da6485eb27c97d8284b6713bc99ebdc94de9
Maxim Farafonov <anderson.utuber@gmail.com>
2023-03-20 15:27:49 +0600

    Implemented few things related to Spring Security, registration and login are yet to be redone

--- src/main/java/com/microgram/project/entity/User.java
+++ src/main/java/com/microgram/project/entity/User.java
@@ -3,10 +3,16 @@ package com.microgram.project.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     private Long id;
     private String name;
     private String username;
@@ -18,4 +24,29 @@ public class User {
     private Integer subsQty;
     @JsonProperty("followers_qty")
     private Integer followersQty;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("FULL"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
