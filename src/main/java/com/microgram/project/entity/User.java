fea4e37b418b37134647f85d4aff71340a215ad4
Maxim Farafonov <anderson.utuber@gmail.com>
2023-06-14 16:25:45 +0600

    test
    Signed-off-by: Maxim Farafonov <anderson.utuber@gmail.com>

--- src/main/java/com/microgram/project/entity/User.java
+++ src/main/java/com/microgram/project/entity/User.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 public class User {
     private Long id;
     private String name;
-    private String usernam;
+    private String username;
     private String email;
     private String password;
     @JsonProperty("post_qty")
