0982f6a7aa9503a5c363a2f477c327fff1111347
Maxim Farafonov <anderson.utuber@gmail.com>
2023-06-14 16:18:57 +0600

    Optimized the imports
    Signed-off-by: Maxim Farafonov <anderson.utuber@gmail.com>

--- src/main/java/com/microgram/project/entity/User.java
+++ src/main/java/com/microgram/project/entity/User.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Collection;
-import java.util.List;
-
 @Getter
 @Setter
 public class User {
